cfbbed59424adf9b7bad0336f9ac719f59252100
Lewinskie <wangaralewis@gmail.com>
2023-07-13 11:47:22 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: sizeof(char)");
+	puts("Size of an int: sizeof(int)");
+	puts("Size of a long long int: sizeof(long long)");
+	puts("Size of a long int: sizeof(long)");
+	puts("Size of a float: sizeof(float)");
+	return (o);
+}
