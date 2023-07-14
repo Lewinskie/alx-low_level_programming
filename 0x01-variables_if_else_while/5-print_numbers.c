4b7081c0a2285b6019a1ac39aef04c8f7730eeb3
Lewinskie <wangaralewis@gmail.com>
2023-07-14 12:49:06 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - program that prints all single digit numbers of base10 starting from 0
+ * Return: 0 if successful, non-zero otherwise
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		puts(x);
+	}
+	return (0);
+}
