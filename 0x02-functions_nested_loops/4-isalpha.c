86ae57d21ccdb2b58228bb73588a79f314275431
Lewinskie <wangaralewis@gmail.com>
2023-07-18 04:22:24 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha(int c) - checks for alphabetic characters
+ * @c: the character to check
+ * Return: if letter, return 1 else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
