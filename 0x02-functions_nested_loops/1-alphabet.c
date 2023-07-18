586726dcf0145c0d506bef33d6690b7292c8218e
Lewinskie <wangaralewis@gmail.com>
2023-07-18 03:25:27 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
-#include <unstd.h>
+#include "main.h"
 /**
- * main - check the code
- * _putchar - writes the character c to stdout
- * @c: the character to write
- * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * main - Entry point
  * Return: Always 0.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar('z');
+		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
