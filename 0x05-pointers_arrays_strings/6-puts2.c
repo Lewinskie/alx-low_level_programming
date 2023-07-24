1b2916eaab73a6d3942accab1263f95257a85027
Lewinskie <wangaralewis@gmail.com>
2023-07-24 12:35:38 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int x = 0;
 
-	for (x = 0; str[x] != '\0'; x += 2)
+	for (x = 0; str[x] != '\0'; x += 1)
 	{
-		_putchar(str[x]);
+		if (x % 2 == 0)
+			_putchar(str[x]);
 	}
 	_putchar('\n');
 }
