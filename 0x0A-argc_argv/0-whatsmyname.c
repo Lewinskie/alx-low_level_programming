28aa3dea556e3647ded12e44a04d50591e3ee5cb
Lewinskie <wangaralewis@gmail.com>
2023-08-04 03:40:50 -0700

    It ain't what they call you, it's what you answer

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  * @argc: count of arguments
- * @argv[]: the commands 
+ * @argv: the commands
  * Return: the command name in index 0
  */
 int main(int argc, char *argv[])
