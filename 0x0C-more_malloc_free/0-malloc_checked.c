e4d9af71b371738030311f24d428495f86efc22b
Lewinskie <wangaralewis@gmail.com>
2023-08-10 05:30:42 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,16 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: input int
- * Return: always 0
+ * Return: always 0i
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
-	
+
 	if (ptr == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(98);
 	}
-	return ptr;
+	return (ptr);
 }
