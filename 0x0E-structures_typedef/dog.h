18a9f902d76396af9bb33af6ac94b4259459041f
Lewinskie <wangaralewis@gmail.com>
2023-08-14 06:03:07 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,10 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Description: Longer description
  */
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
