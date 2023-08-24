765e9cad8871cf725b57f7621cc632f5aa47f376
Lewinskie <wangaralewis@gmail.com>
2023-08-24 03:47:51 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_introduction - Prints the introductory lines.
+ */
+void print_introduction(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
